d22228d5f51ab3da803da1018a0d3d415b2b6dbc
David Francos <xayon>
2020-08-09 00:53:41 +0200

    PEP8

--- src/switchbot/handlers/meter.py
+++ src/switchbot/handlers/meter.py
@@ -15,9 +15,9 @@ async def latest(request):
 
 
 async def start(request):
-
     async def scanner_async(request):
-        scanner = DevScanner(request.rel_url.query.get('device', 'hci0'), int(request.rel_url.query.get('wait_time', 5)))
+        scanner = DevScanner(request.rel_url.query.get('device', 'hci0'),
+                             int(request.rel_url.query.get('wait_time', 5)))
         async for result in scanner.scan():
             request.app['meter_storage'].append(result)
 
